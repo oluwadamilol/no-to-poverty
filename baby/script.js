a5063b37a32792a420c0cc0c1c1670477e89acb8
dazainitiative <dazafoundation@users.noreply.github.com>
2020-02-19 02:48:41 -0800

    Initial commit

---
+++ baby/script.js
@@ -0,0 +1,9 @@
+"hello"
+prompt("Enter username")
+var first=prompt("Enter username")
+var second=("Enter second number")
+
+alert("welcome to daza foundation!")
+if(name=== "Tayo"){
+    alert("hi Tayo")
+}
